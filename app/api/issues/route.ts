6e5920bc5f16308ebc0bae5fb973a014a8866737
peternemeth05 <pnemethasd@gmail.com>
2025-09-26 23:23:57 +0100

    built api for creating issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import { validateHeaderValue } from "node:http";
+import { z } from 'zod';
+
+import prisma from "@/prisma/ client";
+
+
+const createIssueSchema = z.object({ // this is a blueprint that defines what teh incoming data should look like
+    title: z.string().min(1).max(255), // this is the requirement
+    description: z.string().min(1)
+})
+
+export async function POST(request: NextRequest){
+    const body = await request.json();
+    const validation = createIssueSchema.safeParse(body);
+    if (!validation.success)
+        return NextResponse.json(validation.error.errors, {status: 400}) 
+    // this makes sure the input is valid otherwise a 400 error, meaning the client sent invalid data
+
+    const newIssue = await prisma.issue.create({
+        data: {title: body.title, description: body.description}
+    })
+
+    return NextResponse.json(newIssue, {status: 201})
+
+}
